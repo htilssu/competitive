f34f527eb340235116ee69644682d9352581dc49
htilssu <tolashuu@gmail.com>
2025-09-09 00:15:26 +0700

    lại 1 bài medium O(nlog(n))

---
+++ leetcodes/2195_append-k-integers-with-minimal-sum_medium/main.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+#define ll long long
+
+
+class Solution {
+public:
+    long long minimalKSum(vector<int> &nums, int k) {
+        sort(nums.begin(), nums.end());
+        set set(nums.begin(), nums.end());
+
+        ll sum = (ll)k * (k + 1) / 2;
+        for (const __remove_cv(int) i : set) {
+            if (i <= k) {
+                k++;
+                sum += k - i;
+            } else {
+                return sum;
+            }
+        }
+
+        return sum;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector nums{
+        96, 44, 99, 25, 61, 84, 88, 18, 19, 33, 60, 86, 52, 19, 32, 47, 35, 50, 94, 17, 29, 98, 22, 21, 72, 100, 40, 84
+    };
+    cout << sol.minimalKSum(nums, 35) << endl;
+    //794
+    return 0;
+}
